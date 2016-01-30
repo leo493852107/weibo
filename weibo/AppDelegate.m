8a6d65783f17dc428677bea293f80c4b90c3e5ac
leo <leo493852107@163.com>
2016-01-30 16:25:18 +0800

    first commit

---
+++ weibo/AppDelegate.m
@@ -0,0 +1,79 @@
+//
+//  AppDelegate.m
+//  weibo
+//
+//  Created by leo on 16/1/30.
+//  Copyright © 2016年 leo. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // 创建窗口
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    // 创建tabBarVC
+    UITabBarController *tabBarVC = [[UITabBarController alloc] init];
+    
+    tabBarVC.view.backgroundColor = [UIColor redColor];
+    
+    // 管理子控制器
+    // 首页
+    UIViewController *home = [[UIViewController alloc] init];
+    home.view.backgroundColor = [UIColor greenColor];
+    [tabBarVC addChildViewController:home];
+    // 消息
+    UIViewController *message = [[UIViewController alloc] init];
+    message.view.backgroundColor = [UIColor blueColor];
+    [tabBarVC addChildViewController:message];
+    // 发现
+    UIViewController *discover = [[UIViewController alloc] init];
+    discover.view.backgroundColor = [UIColor purpleColor];
+    [tabBarVC addChildViewController:discover];
+    // 我
+    UIViewController *profile = [[UIViewController alloc] init];
+    profile.view.backgroundColor = [UIColor grayColor];
+    [tabBarVC addChildViewController:profile];
+    
+    // 设置窗口的根控制器
+    self.window.rootViewController = tabBarVC;
+    
+    // 显示窗口
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
